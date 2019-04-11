869eb12972d00e86818dafcd33f86aa7b274c557
Vova Space <vova.space@icloud.com>
2019-04-11 07:30:38 +0300

    Add gulpTaskIf

---
+++ index.js
@@ -0,0 +1,36 @@
+const log = require('fancy-log');
+const color = require('ansi-colors');
+
+module.exports = function taskIf(condition, name, fn) {
+  if (typeof fn === 'undefined' && typeof name === 'function') {
+    fn = name;
+    name = undefined;
+  }
+
+  if (typeof condition !== 'boolean') {
+    throw new Error('gulp-task-if: condition must be a boolean.');
+  }
+
+  if (typeof fn !== 'function') {
+    throw new Error('gulp-task-if: wrong \'function\' argument.');
+  }
+
+  if (typeof name !== 'string' && typeof name !== 'undefined') {
+    throw new Error('gulp-task-if: name must be a string.');
+  }
+
+  if (condition) {
+    return fn;
+  }
+
+  function skipped(done) {
+    if (name) {
+      log(`${color.yellow('Skipped')}  '${color.cyan(name)}'`);
+    } else {
+      log(color.yellow('Skipped task'));
+    }
+    done();
+  }
+
+  return skipped;
+};
